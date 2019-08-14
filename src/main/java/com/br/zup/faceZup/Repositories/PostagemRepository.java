24131a7298239867c83b36181069440ab23f55f2
Brenda <brenda.lima@zup.com.br>
2019-08-13 23:18:45 -0300

    Refatoração do face zup

---
+++ src/main/java/com/br/zup/faceZup/Repositories/PostagemRepository.java
@@ -0,0 +1,9 @@
+package com.br.zup.faceZup.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.br.zup.faceZup.Models.PostagemModel.Postagem;
+
+public interface PostagemRepository extends CrudRepository<Postagem, Integer>{
+
+}
